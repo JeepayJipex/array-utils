74c9513d1f7bb10ad909cfecec2703c0cce52b5c
JeepayJipex <jean.mariette@biwyze.fr>
2020-09-27 18:02:28 +0200

    changed getWhere jsdoc comments

--- src/method/get-where.ts
+++ src/method/get-where.ts
@@ -2,30 +2,39 @@ import { operators } from '@helpers/Operators'
 import { ComparisonOperators as ComparisonOperator } from '@custom-types/comparison-operators'
 
 /**
- * Retrieve elements that matches comparison in the given array
- * @param arr The array to search inside
- * @param operator the comparison operator to use
- * @param value the value to compare with
+ * Gets where items satisfy the passed condition
+ * @template T Types present in array
+ * @template U Type of comparison value passed
+ * @param arr The array to search in
+ * @param operator The comparison operator to use
+ * @param value The value to compare with
+ * @returns items where comparison is true
  */
 export function getWhere<T, U> (arr: Array<T>, operator: ComparisonOperator, value: U): Array<T|U> {
   return arr.filter(item => operators[operator](item, value))
 }
 
 /**
- * Retrieve the first element that matches comparison in the given array
- * @param arr The array to search inside
- * @param operator the comparison operator to use
- * @param value the value to compare with
+ * Gets where first item satisfy the passed condition
+ * @template T Types present in array
+ * @template U Type of comparison value passed
+ * @param arr The array to search in
+ * @param operator The comparison operator to use
+ * @param value The value to compare with
+ * @returns first item where comparison is true
  */
 export function getWhereFirst<T, U> (arr: Array<T>, operator: ComparisonOperator, value: U): T|U|undefined {
   return arr.find(item => operators[operator](item, value))
 }
 
 /**
- * Retrieve the last element that matches comparison in the given array
- * @param arr The array to search inside
- * @param operator the comparison operator to use
- * @param value the value to compare with
+ * Gets last item that satisfy the passed condition
+ * @template T Types present in array
+ * @template U Type of comparison value passed
+ * @param arr The array to search in
+ * @param operator The comparison operator to use
+ * @param value The value to compare with
+ * @returns last item where comparison is true
  */
 export function getWhereLast<T, U> (arr: Array<T>, operator: ComparisonOperator, value: U): T|U|undefined {
   return arr.reverse().find(item => operators[operator](item, value))
